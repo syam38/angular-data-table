77cc4fcd09a97e1924bed2b730da30b0d93c8c30
syam38 <syampradeep2b6@gmail.com>
2020-07-13 17:18:21 +0530

    Added default rows and columns

--- src/app/dynamic-data-table.ts
+++ src/app/dynamic-data-table.ts
@@ -1,5 +1,5 @@
 import {DataSource} from '@angular/cdk/collections';
-import {Component, ViewChild, ElementRef} from '@angular/core';
+import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 
 let ELEMENT_DATA: any[] = [
@@ -10,23 +10,38 @@ let ELEMENT_DATA: any[] = [
   styleUrls: ['dynamic-data-table.css'],
   templateUrl: 'dynamic-data-table.html',
 })
-export class DynamicDataTable {
-  displayedColumns: string[] = ['Student'];
+export class DynamicDataTable  implements OnInit{
+  displayedColumns: string[] = ['Student', 'Social', 'Science', 'Maths'];
   dataSource = new ExampleDataSource();
   columnName='';
   rowName = '';
+  rows = ['x', 'y', 'z', 'a'];
   @ViewChild('tableContainer', { read: ElementRef }) tableContainer: ElementRef;
 
+  ngOnInit() {
+         this.updateTable();
+  }
+
   addRow() {
-    let cols = this.displayedColumns.map((c) => { c: ''} );
-    ELEMENT_DATA.push({Student: this.rowName, ...cols });
-    this.dataSource = new ExampleDataSource();
-     console.log('dsd', this.dataSource)
+    this.rows.push(this.rowName);
+     console.log('dsd', this.dataSource);
+      let cols = this.displayedColumns.map((c) => { c: ''} );
+     ELEMENT_DATA.push({ Student: this.rowName, ...cols });
+      this.dataSource = new ExampleDataSource();
   }
 
   addColumn() {
     this.displayedColumns.push(this.columnName);
     ELEMENT_DATA.forEach((d) => d[this.columnName] = '');
+    this.dataSource = new ExampleDataSource();
+  }
+
+  updateTable() {
+    let cols = this.displayedColumns.map((c) => { c: ''} );
+    this.rows.forEach((c) => {
+       ELEMENT_DATA.push({ Student: c, ...cols });
+    });
+    this.dataSource = new ExampleDataSource();
   }
 
 }
